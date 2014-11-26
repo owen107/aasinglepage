b465a57e68d3a0a42d8fa8dc1ff5e70459fe8da5
owen107 <owenxu107@gmail.com>
2014-11-25 23:54:24 -0500

    Added image slider in umich section

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	
+    // set up the image hover effect in welcome section
 	$('#welcome').find('.gallery img').hover(function() {
          var curImgLoc = $(this).attr('src');
          var hoverImgLoc = curImgLoc.substr(0, curImgLoc.length-4) + "_h.jpg";
@@ -8,4 +10,26 @@ $(document).ready(function(){
          var newImgLoc = curImgLoc.substr(0, curImgLoc.length-6) + ".jpg";
          $(this).attr('src', newImgLoc);
 	});
+    
+    // set up the image slider in umich section
+    // configuration
+    var width = 650;
+    var animationSpeed = 1000;
+    var pause = 4000;
+    var currentSlide = 1;
+
+    // cache DOM
+    var $slider = $('#slider');
+    var $sliderContainer = $slider.find('.slides');
+    var $slide = $sliderContainer.find('.slide');
+    
+	setInterval(function() {
+		$sliderContainer.animate({'margin-left': '-=' + width + 'px'}, animationSpeed, function() {
+			currentSlide++;
+			if (currentSlide === $slide.length) {
+				currentSlide = 1;
+				$sliderContainer.css('margin-left', 0);
+			}
+		});
+	}, pause);
 });
